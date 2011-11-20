e842dfb0b4e10756cb8616eb72d85e350d4fe891
Harry Halladay <harryrh@me.com>
2011-11-20 15:09:26 -0600

    Refactor, Add week font

--- bin/get_scores_nfl.py
+++ bin/get_scores_nfl.py
@@ -33,45 +33,57 @@ def normalize(string):
 
 class Team():
     
-    def __init__(self, name, score=None, record=None, image=None):
+    def __init__(self, name, scores=None, record=None, game_record=None, winner=None, possession=None, image=None):
 
         self.name = name
-        self.score = score
+        self.scores = scores
         self.record = record
+        self.game_record = game_record
+        self.winner = winner
+        self.possession = possession
         self.image = image
 
     def __str__(self):
 
         info = []
         info.append('<Team')
-        info.append('name="%s"' % self.name)
-        info.append('score="%s"' % self.score)
-        info.append('record="%s"' % self.record)
-        info.append('image="%s"' % self.image)
+        info.append('name=%s' % self.name)
+        info.append('scores=%s' % ','.join(self.scores))
+        info.append('record=%s' % self.record)
+        info.append('game_record=%s' % self.game_record)
+        info.append('possession=%s' % self.possession)
+        info.append('winner=%s' % self.winner)
+        info.append('image=%s' % self.image)
+        info.append('>')
 
         return " ".join(info)
 
 class Game():
 
-    def __init__(self, away_team, home_team,
-            status=None, tv=None, headline=None, date=None):
+    def __init__(self, away_team, home_team, type,
+            status=None, tv=None, headline=None, date=None, lastplay=None):
 
         self.away_team = away_team
         self.home_team = home_team
+        self.type = type
         self.status = status
         self.tv = tv
         self.headline = headline
         self.date = date
+        self.lastplay = lastplay
 
     def __str__(self):
 
         info = []
         info.append('<Game')
-        info.append('away_team="%s"' % self.away_team)
-        info.append('home_team="%s"' % self.home_team)
-        info.append('status="%s"' % self.status)
-        info.append('tv="%s"' % self.tv)
-        info.append('headline="%s"' % self.headline)
+        info.append('away_team=%s' % self.away_team)
+        info.append('home_team=%s' % self.home_team)
+        info.append('type=%s' % self.type)
+        info.append('date=%s' % self.date)
+        info.append('status=%s' % self.status)
+        info.append('tv=%s' % self.tv)
+        info.append('headline=%s' % self.headline)
+        info.append('lastplay=%s' % self.lastplay)
         info.append('>')
 
         return " ".join(info)
@@ -88,6 +100,63 @@ def get_font(name, args, default_font):
     return font
 
 #-------------------------------------------------------------------------------
+
+def render_game(game, fonts, fontcolor):
+
+    game_images = []
+
+    away_image = game.away_team.image
+    home_image = game.home_team.image
+
+    upcoming = game.type.startswith('preview')
+
+    if upcoming:
+        ar = game.away_team.record
+        hr = game.home_team.record
+        ari = text_as_image(ar, font=fonts['record'], fill=fontcolor)
+        hri = text_as_image(hr, font=fonts['record'], fill=fontcolor)
+        away_image = vertical_montage([game.away_team.image, ari], halign='center')
+        home_image = vertical_montage([game.home_team.image, hri], halign='center')
+
+    icons = horizontal_montage([away_image, home_image], spacing=args.spacing)
+    game_images.append(icons)
+
+    if upcoming:
+        delta = game.date - localtz.localize(datetime.now())
+        game_images.append(text_as_image(game.date.strftime(args.date_format), font=fonts['date'], fill=fontcolor))
+        if delta.days >= 7:
+            game_images.append(text_as_image(game.date.strftime('%b %m, %Y'), font=fonts['date'], fill=fontcolor))
+        game_images.append(text_as_image(game.tv, font=fonts['tv'], fill=fontcolor))
+    else:
+        if args.quarters:
+            # Quarters as text
+            (w, h) = icons.size
+
+            asi = [text_as_image(s, font=fonts['quarter'], fill=fontcolor) for s in game.away_team.scores]
+            hsi = [text_as_image(s, font=fonts['quarter'], fill=fontcolor) for s in game.home_team.scores]
+
+            qis = []
+            for x in zip(asi, hsi):
+                qis.append(vertical_montage(list(x), halign='right', sameheight=True))
+                
+            quarters_image = horizontal_montage(qis, min_width=w/6, halign='right')
+
+            game_images.append(quarters_image)
+        else:
+            # Summary score
+            game_images.append(text_as_image("%s - %s" % (game.away_team.scores[5], game.home_team.scores[5]),
+                font=fonts['default'], fill=fontcolor))
+        game_images.append(text_as_image(game.status, font=fonts['default'], fill=fontcolor))
+        if game.headline and args.headline:
+            game_images.append(text_as_image(game.headline, font=fonts['headline'], fill=fontcolor))
+
+    return vertical_montage(game_images, halign='center', spacing=0)
+
+def render_games(games, fonts, fontcolor):
+    game_images = []
+    for game in games:
+        game_images.append(render_game(game, fonts, fontcolor))
+    return game_images
 
 conf_parser = ArgumentParser(add_help=False)
 conf_parser.add_argument('-f', '--config-file', dest='config_file', metavar='FILE',
@@ -120,6 +189,7 @@ parser.add_argument('--timestamp', dest='timestamp', action='store_true', defaul
 parser.add_argument('--timestamp-format', dest='timestamp_format', default='%m/%d %H:%M',
         help='Format for timestamp (strftime)')
 parser.add_argument('--timezone', dest='timezone', default='US/Central', help='Local Time Zone')
+parser.add_argument('--date-format', dest='date_format', default='%a %l:%M %p %Z', metavar='FORMAT', help='Date format')
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-n', '--next-week', dest='next_week', action='store_true', help='Next week')
@@ -174,132 +244,110 @@ if args.next_week or args.prev_week or args.week:
 container_re = re.compile('\d+-gameContainer')
 soup = BeautifulSoup(html)
 
-def parse_team_block(block):
-
-    name = block.contents[2].contents[0].a.text
-    record = block.contents[2].contents[1].text
-
-    score_block = block.find('ul', attrs = { 'class': 'score' })
-    score = [s.text.replace('&nbsp;', '') for s in score_block.contents]
-
-    image = Image.open(vargs['image.' + normalize(name)]).convert('RGBA')
-
-    return Team(name, score=score, record=record, image=image)
-
 upcoming_re = re.compile('(\d+):(\d+) ([AP]M) ET')
 date_re = re.compile('(\w+),\s+(\w+)\s+(\d+),\s+(\d+)')
 
+def parse_team_block(block, blockid, ahprefix):
+
+    name_b = block.find('p', 'team-name')
+    name = name_b.a.text
+
+    winner_b = block.find('div', id='%s-%sWinner' % (blockid, ahprefix))
+    winner = winner_b['style'].endswith('display:block')
+
+    possession_b = block.find('span', id='%s-%sPossession' % (blockid, ahprefix))
+    possession = possession_b['style'].endswith('display:inline;')
+
+    scores_b = block.find('ul', 'score')
+    scores = tuple([li.text.replace('&nbsp;', '') for li in scores_b.findAll('li')])
+
+    record_b = block.find('p', 'record')
+    record_s = record_b.text
+    (record, game_record) = record_s[1:-1].split(', ')
+
+    return Team(name, winner=winner, possession=possession, scores=scores, record=record, game_record=game_record)
+
+def get_image(team, args):
+    return Image.open(args['image.' + normalize(team.name)])
+
 games = []
 dates = soup.findAll('h4', 'games-date')
-for i, d in enumerate(dates):
-    dm = date_re.match(d.text)
+for date_block in dates:
+    dm = date_re.match(date_block.text)
     (gweekday, gmonth_name, gmonth_day, gyear) = dm.groups()
     game_date = date(int(gyear), monthnum[gmonth_name], int(gmonth_day))
-    for j, c in enumerate(d.nextSibling.contents):
-        for i, g in enumerate(c.findAll('div', id=container_re)):
-            status_block = g.find('div', attrs = { 'class': 'game-status' })
-            status = status_block.p.text.replace('&nbsp;', '').strip()
 
-            upcoming = upcoming_re.match(status)
-            # Upcoming game -- parse the time into an actual time
-            if upcoming:
-                hour = int(upcoming.group(1))
-                minute = int(upcoming.group(2))
-                ampm = upcoming.group(3)
-                if ampm == 'PM' and int(hour) < 12:
-                    hour = hour + 12
-                game_time = time(int(hour), int(minute), tzinfo=eastern)
-                game_date = datetime.combine(game_date, game_time).astimezone(localtz)
+    for c in date_block.nextSibling.contents:
+        for game_block in c.findAll('div', id=container_re):
+            blockid = game_block['id'].split('-')[0]
+            type = game_block['class'].split(' ')[3]
 
-            tv_block = g.find('div', attrs = { 'class': 'tv' })
+            status_block = game_block.find('p', id='%s-statusText' % blockid)
+            status = status_block.text.replace('&nbsp;', '').strip()
+
+            headline = None
+            lastplay = None
+
+            if type == 'preview':
+                upcoming = upcoming_re.match(status)
+                # Upcoming game -- parse the time into an actual time
+                if upcoming:
+                    hour = int(upcoming.group(1))
+                    minute = int(upcoming.group(2))
+                    ampm = upcoming.group(3)
+                    if ampm == 'PM' and int(hour) < 12:
+                        hour = hour + 12
+                    game_time = time(int(hour), int(minute), tzinfo=eastern)
+                    game_date = datetime.combine(game_date, game_time).astimezone(localtz)
+
+            elif type == 'in-game':
+
+                lastplay_block = game_block.find('div', id='%s-lastPlayText' % blockid)
+                lastplay = lastplay_block.text.replace('&nbsp;', '')
+                    
+                pass
+
+            elif type == 'final-state':
+
+                headline_block = game_block.find('div', id='%s-recapHeadline' % blockid)
+                headline = headline_block.text.replace('&nbsp;', '').strip()
+
+                pass
+
+            tv_block = game_block.find('div', id='%s-preTV' % blockid)
             tv = tv_block.p.text.replace('&nbsp;', '').strip()
 
-            headline_block = g.find('div', attrs = { 'class': 'headline' })
-            headline = headline_block.text.replace('&nbsp;', '').strip()
+            away_block = game_block.find('div', 'team visitor')
+            away_team = parse_team_block(away_block, blockid, 'a')
+            away_team.image = get_image(away_team, vargs)
 
-            away_block = g.find('div', attrs = { 'class': 'team visitor' })
-            away_team = parse_team_block(away_block)
+            home_block = game_block.find('div', 'team home')
+            home_team = parse_team_block(home_block, blockid, 'h')
+            home_team.image = get_image(home_team, vargs)
 
-            home_block = g.find('div', attrs = { 'class': 'team home' })
-            home_team = parse_team_block(home_block)
-
-            game = Game(away_team, home_team, status=status, tv=tv, headline=headline, date=game_date)
+            game = Game(away_team, home_team, type=type, status=status, tv=tv, headline=headline, date=game_date, lastplay=lastplay)
 
             games.append(game)
 
-
+fonts = {}
 try:
-    font = ImageFont.truetype(args.font, args.fontsize)
+    fonts['default'] = ImageFont.truetype(args.font, args.fontsize)
 except:
-    font = ImageFont.load_default()
+    fonts['default'] = ImageFont.load_default()
 
-print 'font:', font
-
-record_font = get_font('record', vargs, font)
-print 'record font:', record_font
-headline_font = get_font('headline', vargs, font)
-date_font = get_font('date', vargs, font)
-quarter_font = get_font('quarter', vargs, font)
-tv_font = get_font('tv', vargs, font)
-timestamp_font = get_font('timestamp', vargs, font)
+fonts['record'] = get_font('record', vargs, fonts['default'])
+fonts['headline'] = get_font('headline', vargs, fonts['default'])
+fonts['date'] = get_font('date', vargs, fonts['default'])
+fonts['quarter'] = get_font('quarter', vargs, fonts['default'])
+fonts['tv'] = get_font('tv', vargs, fonts['default'])
+fonts['timestamp'] = get_font('timestamp', vargs, fonts['default'])
+fonts['week'] = get_font('week', vargs, fonts['default'])
 
 fontcolor = 'white'
 if args.fontcolor:
     fontcolor=args.fontcolor
 
-images = []
-for g in games:
-    gi = []
-
-    upcoming = upcoming_re.match(g.status)
-
-    away_image = g.away_team.image
-    home_image = g.home_team.image
-    if upcoming:
-        ar = g.away_team.record[1:g.away_team.record.find(',')]
-        hr = g.home_team.record[1:g.home_team.record.find(',')]
-        ari = text_as_image(ar, font=record_font, fill=fontcolor)
-        hri = text_as_image(hr, font=record_font, fill=fontcolor)
-        away_image = vertical_montage([g.away_team.image, ari], halign='center')
-        home_image = vertical_montage([g.home_team.image, hri], halign='center')
-
-    icons = horizontal_montage([away_image, home_image], spacing=args.spacing)
-    gi.append(icons)
-
-
-    if upcoming:
-        delta = g.date - datetime.now().replace(tzinfo=localtz)
-        gi.append(text_as_image(g.date.strftime('%a %l:%M %p %Z'), font=date_font, fill=fontcolor))
-        if delta.days >= 7:
-            gi.append(text_as_image(g.date.strftime('%b %m, %Y'), font=date_font, fill=fontcolor))
-        gi.append(text_as_image(g.tv, font=tv_font, fill=fontcolor))
-    else:
-        if args.quarters:
-            # Quarters as text
-            (w, h) = icons.size
-
-            asi = [text_as_image(s, font=quarter_font, fill=fontcolor) for s in g.away_team.score]
-            hsi = [text_as_image(s, font=quarter_font, fill=fontcolor) for s in g.home_team.score]
-
-            qis = []
-            for x in zip(asi, hsi):
-                qis.append(vertical_montage(list(x), halign='right', sameheight=True))
-                
-            quarters_image = horizontal_montage(qis, min_width=w/6, halign='right')
-
-            gi.append(quarters_image)
-        else:
-            # Summary score
-            gi.append(text_as_image("%s - %s" % (g.away_team.score[5], g.home_team.score[5]),
-                font=font, fill=fontcolor))
-        gi.append(text_as_image(g.status, font=font, fill=fontcolor))
-        if g.headline and args.headline:
-            gi.append(text_as_image(g.headline, font=headline_font, fill=fontcolor))
-
-    image = vertical_montage(gi, halign='center', spacing=0)
-
-    images.append(image)
-
+images = render_games(games, fonts, fontcolor)
 
 if args.slideshow:
     if not os.path.exists(args.slideshow):
@@ -316,14 +364,14 @@ if args.slideshow:
             image = image.convert('LA')
         image.save(filename)
 
-week_im = text_as_image("WEEK %s" % week, font=font, fill=fontcolor)
+week_im = text_as_image("WEEK %s" % week, font=fonts['week'], fill=fontcolor)
 
 if args.timestamp:
     now = localtz.localize(datetime.now())
-    now_image = text_as_image(now.strftime(args.timestamp_format), font=timestamp_font, fill=fontcolor)
+    now_image = text_as_image(now.strftime(args.timestamp_format), font=fonts['timestamp'], fill=fontcolor)
 
 if not images:
-    images = [text_as_image('No Games', font=font, fill=fontcolor)]
+    images = [text_as_image('No Games', font=fonts['default'], fill=fontcolor)]
 
 if args.vertical:
     montage = vertical_montage(images, spacing=max(int(args.vpad),0), halign='center')
