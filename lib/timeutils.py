adf53281532d3adecca5e4a704d15f1a356825ce
Harry Halladay <harryrh@me.com>
2012-07-01 15:04:54 -0500

    add duration_upper

--- lib/timeutils.py
+++ lib/timeutils.py
@@ -27,3 +27,31 @@ def duration_human(seconds):
             duration.append('%d Second' % seconds + 's'*(seconds != 1))
     return ', '.join(duration)
 
+def duration_upper(seconds):
+    seconds = long(round(seconds))
+    minutes, seconds = divmod(seconds, 60)
+    hours_f = float(minutes) / 60
+    hours, minutes = divmod(minutes, 60)
+    days_f = float(hours) / 24
+    days, hours = divmod(hours, 24)
+    years_f = float(days) / 365.242199
+    years, days = divmod(days, 365.242199)
+ 
+    minutes = long(minutes)
+    hours = long(hours)
+    days = long(days)
+    years = long(years)
+ 
+    duration = []
+    if years > 0:
+        return '%0.1fy' % years_f
+    if days > 0:
+        return '%0.1fd' % days_f
+    if hours > 0:
+        return '%0.1fh' % hours_f
+    if minutes > 0:
+        return '%dm' % minutes
+    if seconds > 0:
+        return '%ds' % seconds
+
+    return None
